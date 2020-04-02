06994f21629eb0d757bffb908c695fcc5b82f9c7
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-02 06:52:56 -0700

    added more spans, established consts, starting to work out eventlisteners

--- r-p-s.js
+++ r-p-s.js
@@ -1,39 +1,61 @@
 // Import from other js 
-import getRandomThrow from '../get-random-throw.js';
-import checkResults from '../get-random-throw.js';
+import getRandomThrow from './get-random-throw';
+import checkResults from './check-results';
 
 // Establish the DOM
 const playButton = document.getElementById('play-button');
 const resetButton = document.getElementById('reset-button');
 const radios = document.querySelectorAll('input');
+const winsSpan = document.getElementById('wins-count');
+const lossesSpan = document.getElementById('losses-count');
+const drawsSpan = document.getElementById('draws-count');
 
 // Establishing the Const for the 'checked' in the DOM
 const rocksInput = radios[0];
 const paperInput = radios[1];
 const scissorsInput = radios[2];
 
+let winsCount = 0; 
+let lossesCount = 0; 
+let drawsCount = 0; 
 
+const updateMatchScores = () => {
+    winsSpan.textContent = winsCount; 
+    lossesSpan.textContent = lossesCount; 
+    drawsCount.textContent = drawsCount; 
+}; 
 
 // Event Listeners 
 // Play Button
 playButton.addEventListener('click', () => {
-    console.log ('rocks is checked', rocksInput.checked);
-    console.log ('paper is checked', paperInput.checked);
-    console.log ('scissors is checked', scissorsInput.checked);
+    const selectedChoice = document.querySelector('input[type=radio]:checked');
+    const computerThrow = getRandomThrow; 
+    const playerThrow = selectedChoice.nodeValue; 
+    const results = checkResults(playerThrow, computerThrow); 
+
+    if (results === 'draw') { 
+        drawsCount++; 
+
+    }
+
+
+
+
 });
 
-// Reset Button 
-resetButton.addEventListener('click', () => {
-
-}); 
 
 
+// // Reset Button 
+// resetButton.addEventListener('click', () => {
 
-// if (rocksInput.checked){
-//     userChoice = 'rock';
-// } else if (rocksInput) {
-//     userChoice = 'paper'; 
-// } else {
-//     userChoice = 'scissors';     
-// }
-// }
+// }); 
+
+
+
+ if (rocksInput.checked){
+    userChoice = 'rock';
+} else if (rocksInput) {
+    userChoice = 'paper'; 
+} else {
+    userChoice = 'scissors';     
+}
