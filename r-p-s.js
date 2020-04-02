307eb722a4345051d9ce77feae6c16fcfc9eb3da
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-02 07:34:59 -0700

    counters now work, but do not display what they are also fixed imported files that were not linking into main js file

--- r-p-s.js
+++ r-p-s.js
@@ -1,19 +1,21 @@
 // Import from other js 
-import getRandomThrow from './get-random-throw';
-import checkResults from './check-results';
+import getRandomThrow from './get-random-throw.js';
+import checkResults from './check-results.js';
 
 // Establish the DOM
 const playButton = document.getElementById('play-button');
-const resetButton = document.getElementById('reset-button');
-const radios = document.querySelectorAll('input');
+// const resetButton = document.getElementById('reset-button');
+// const radios = document.querySelectorAll('input');
 const winsSpan = document.getElementById('wins-count');
 const lossesSpan = document.getElementById('losses-count');
 const drawsSpan = document.getElementById('draws-count');
+const winsLosses = document.getElementById('message');
+const currentRoundResults = document.getElementById('result');
 
-// Establishing the Const for the 'checked' in the DOM
-const rocksInput = radios[0];
-const paperInput = radios[1];
-const scissorsInput = radios[2];
+// // Establishing the Const for the 'checked' in the DOM
+// const rocksInput = radios[0];
+// const paperInput = radios[1];
+// const scissorsInput = radios[2];
 
 let winsCount = 0; 
 let lossesCount = 0; 
@@ -22,25 +24,36 @@ let drawsCount = 0;
 const updateMatchScores = () => {
     winsSpan.textContent = winsCount; 
     lossesSpan.textContent = lossesCount; 
-    drawsCount.textContent = drawsCount; 
+    drawsSpan.textContent = drawsCount; 
 }; 
 
 // Event Listeners 
 // Play Button
 playButton.addEventListener('click', () => {
     const selectedChoice = document.querySelector('input[type=radio]:checked');
-    const computerThrow = getRandomThrow; 
-    const playerThrow = selectedChoice.nodeValue; 
+    const computerThrow = getRandomThrow(); 
+    const playerThrow = selectedChoice.value; 
     const results = checkResults(playerThrow, computerThrow); 
 
     if (results === 'draw') { 
         drawsCount++; 
-
+        updateMatchScores(); 
+        winsLosses.textContent = 'Its a draw! The computer chose: ${computerThrow}'; 
+        currentRoundResults.style.visibility = 'visible'; 
+        
     }
-
-
-
-
+    else if (results === 'win') { 
+        winsCount++; 
+        updateMatchScores(); 
+        winsLosses.textContent = 'Congratulations, you won! The computer chose: ${computerThrow}'; 
+        currentRoundResults.style.visibility = 'visible'; 
+    }
+    else if (results === 'lose') { 
+        lossesCount++; 
+        updateMatchScores(); 
+        winsLosses.textContent = "I'm sorry, but you lost this round! The computer chose: ${computerThrow}"; 
+        currentRoundResults.style.visibility = 'visible'; 
+    }
 });
 
 
@@ -52,10 +65,10 @@ playButton.addEventListener('click', () => {
 
 
 
- if (rocksInput.checked){
-    userChoice = 'rock';
-} else if (rocksInput) {
-    userChoice = 'paper'; 
-} else {
-    userChoice = 'scissors';     
-}
+//  if (rocksInput.checked){
+//     userChoice = 'rock';
+// } else if (rocksInput) {
+//     userChoice = 'paper'; 
+// } else {
+//     userChoice = 'scissors';     
+// }
